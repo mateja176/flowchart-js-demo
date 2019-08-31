f06e3b1eb2ec7a5a75eebc1bcc617d0f0dd479f7
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-01 00:22:41 +0200

    feat: adds flowchart.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
+// @ts-ignore
+import flowchart from 'flowchart.js';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  React.useEffect(() => {
+    const diagram = flowchart.parse(
+      'st=>start: Start:>http://www.google.com[blank]\n' +
+      'e=>end:>http://www.google.com\n' +
+      'op1=>operation: My Operation:$myFunction\n' +
+      'op2=>operation: Stuff|current\n' +
+      'sub1=>subroutine: My Subroutine\n' +
+      'cond=>condition: Yes \n' + // use cond(align-next=no) to disable vertical align of symbols below
+      'or No?\n:>http://www.google.com\n' +
+      'c2=>condition: Good idea|rejected\n' +
+      'io=>inputoutput: catch something...|request\n' +
+      '\n' +
+      'st->op1(right)->cond\n' +
+      'cond(yes, right)->c2\n' + // conditions can also be redirected like cond(yes, bottom) or cond(yes, right)
+      'cond(no)->sub1(left)->op1\n' + // the other symbols too...
+        'c2(true)->io->e\n' +
+        'c2(false)->op2->e', //allow for true and false in conditionals
+    );
+    diagram.drawSVG('diagram');
+  }, []);
+  return <div className="App">FlowchartJS</div>;
+};
 
 export default App;
